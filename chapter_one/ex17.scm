ead75b8f8511c1e25377b89a35d71c8d60de8fc0
neatrbk <kenneth.reiswig@gmail.com>
2018-08-12 18:50:21 -0700

    finished 1.7

--- chapter_one/ex17.scm
+++ chapter_one/ex17.scm
@@ -1,4 +1,15 @@
-(define (abs x) 
+;; the origanal procedure is inaccurate for very samll numbers because we only
+;; have precision to .0001, meaining any thing smaller than .0001 would pass the
+;; good-enough? test right away, returning and inaccurate result
+
+;; large numbers would be inaccurate simply because we cannot trust the accracy
+;; of
+
+---------------------------------------------------------------------------------
+
+
+
+(define (abs x)
   (if (< x 0) (- x) x)
 )
 
@@ -14,25 +25,26 @@
 	(* x x)
 )
 
-;; Original procedure 
+;; Original procedure
 
 ;;(define (good-enough? guess x)
 ;;	(< (abs (- (square guess) x))  0.00001))
 ;;)
 
-(define (good-enough? guess x)
-	(= (abs(- (square guess)x)) 0)
+(define (good-enough? guess last)
+  (< (abs(- guess last)) .00001)
 )
 
 (define (sqrt x)
-	(sqrt-iter 1.0 x)
+	(sqrt-iter 1.0 0 x)
 )
 
-(define (sqrt-iter guess x)
-	(if (good-enough? guess x)
+(define (sqrt-iter guess last x)
+	(if (good-enough? guess last)
 		guess
-	(sqrt-iter (improve guess x) x)
+	(sqrt-iter (improve guess x) guess x)
 	)
 )
 
-(sqrt .000000000000000000000000000000000000000000000000025)
+
+(sqrt 81000)
